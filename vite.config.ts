31bf1ebff4d0fc9075ee55e719bca659fba51bb4
yazid <94212436+nvkq@users.noreply.github.com>
2025-04-28 23:27:20 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+// venshort-improved-new/vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // !!! أضف هذا السطر !!!
+  base: '/Venshort2/', // استبدل Venshort2 باسم مستودعك إذا كان مختلفاً
   plugins: [react()],
 })
